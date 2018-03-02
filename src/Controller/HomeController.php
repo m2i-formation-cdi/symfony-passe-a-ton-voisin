3ee79ee16d975c297281aeba83bd254b67bb5033
moi <moi@moi.com>
2018-03-02 13:02:57 +0100

    Log des accès sur HomeController:index pour tester l'autowiring de Symfony

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,7 +14,9 @@ class HomeController extends Controller
      * @Route("/", name="homepage")
      * @return Response
      */
-    public function indexAction(){
+    public function indexAction(LoggerInterface $logger){
+
+        $logger->info("Je suis passé par la maison",[self::class]);
         return $this->render("home/index.html.twig", []);
     }
 

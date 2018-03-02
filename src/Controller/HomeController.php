739885233d491a203e4c7a2249de77f8101b7cb1
moi <moi@moi.com>
2018-03-02 11:23:43 +0100

    Ajout du contrôleur HomeController

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     * @return Response
+     */
+    public function indexAction(){
+        return new Response("Hello symfony 4");
+    }
+
+}

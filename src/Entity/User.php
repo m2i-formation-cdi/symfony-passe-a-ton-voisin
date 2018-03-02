99fb9283c9d14cc30a2ff5a6437cc36a83314e09
moi <moi@moi.com>
2018-03-02 13:31:36 +0100

    Installation de Doctrine et maker Création d'une entité User basique Migration sur la base de données

---
+++ src/Entity/User.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="users")
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    // add your own fields
+}
